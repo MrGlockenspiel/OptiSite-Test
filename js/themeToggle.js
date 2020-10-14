ca83235936439f0b8d51896e08e183a812fb5bda
MrGlockenspiel <garretttijsma@gmail.com>
2020-10-14 18:47:22 -0400

    Delete themeToggle.js

--- js/themeToggle.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('checkbox').addEventListener('change', function(event){
-  (event.target.checked) ? document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
-});
